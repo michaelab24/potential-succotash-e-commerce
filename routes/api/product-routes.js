dda89d82e868782f14df9fb5269b8a4b7877c449
michaelab24 <michaelbarnes459@yahoo.com>
2021-07-13 12:27:12 -0500

    database models done, working on routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category, {
+        model: Tag, 
+        through: ProductTag
+      }
+    ]
+  })
+  .then(products => res.json(products))
+  .catch(err => res.status(500).json(err))
 });
 
 // get one product
